1c42b865fd5ff960cc604690f90463d47c097607
MinhTrinh04 <minhfananime@gmail.com>
2025-12-20 14:10:20 +0700

    Update all Hand models and Btn

---
+++ Assets/Lecture/Script/UIFaceCamera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class UIFaceCamera : MonoBehaviour
+{
+    [Header("Settings")]
+    public bool lockYAxis = true;
+
+    void Update()
+    {
+        if (Camera.main == null) return;
+
+        Vector3 targetPosition = Camera.main.transform.position;
+
+        if (lockYAxis)
+        {
+            targetPosition.y = transform.position.y;
+        }
+
+        transform.LookAt(targetPosition);
+
+        //transform.Rotate(0, 180, 0);
+    }
+}
